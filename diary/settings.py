bb13f1e1cc67199a73e7065cc6695d804f31534f
galeisan <galeeva.leisan.27.09@yandex.ru>
2023-04-27 23:46:09 +0300

    add postgres connection

--- diary/settings.py
+++ diary/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'diary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'diary',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
